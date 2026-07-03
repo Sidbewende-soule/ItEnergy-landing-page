a24ad47b66ba55f3c486e46c386f023660fad974
romafoxz <bationor77@gmail.com>
2026-07-03 11:12:56 +0000

    Creation de la page Contact et de son contenu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home';
+import { Contact } from './pages/contact/contact';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'contact', component: Contact },
   { path: '**', redirectTo: '' }
 ];
