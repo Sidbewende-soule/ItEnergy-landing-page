d06f8b464fe377da4eb0c11d47bbb6e48bf0d36e
Dominique Soulé <sidbewendesoule@gmail.com>
2026-07-03 12:26:32 +0000

    Ajout de la page apropos -base

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home';
 import { Contact } from './pages/contact/contact';
+import { Apropos } from './pages/apropos/apropos';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'contact', component: Contact },
+  { path: 'apropos', component: Apropos },
   { path: '**', redirectTo: '' }
 ];
