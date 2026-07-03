4bd9ae0367f36e5a4e7afb38498ed249bcd9a832
romafoxz <bationor77@gmail.com>
2026-07-03 13:33:33 +0000

    creation de la page solution

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home';
 import { Contact } from './pages/contact/contact';
 import { Apropos } from './pages/apropos/apropos';
+import { Solutions } from './pages/solutions/solutions';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'contact', component: Contact },
   { path: 'apropos', component: Apropos },
+  { path: 'solutions', component: Solutions },
   { path: '**', redirectTo: '' }
 ];
